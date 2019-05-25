1c29afb3e85cb4b9263a63fb6e992ae7cd92bd5f
Nazar Laba <nazz.fardo@gmail.com>
2019-05-25 18:38:28 +0300

    add

---
+++ precache-manifest.57c372b0aaed2005ee89c51e499e14bf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf07e7163b524548f63a050a933ee2a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c683afa11bd23598fd1",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "40d30c87705a4cf52c25",
+    "url": "/static/css/main.c4720937.chunk.css"
+  },
+  {
+    "revision": "9c683afa11bd23598fd1",
+    "url": "/static/js/2.456a1ac8.chunk.js"
+  },
+  {
+    "revision": "40d30c87705a4cf52c25",
+    "url": "/static/js/main.cbe980ed.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ca1602812e03a814b575aa238fc06cdc",
+    "url": "/static/media/Contact-Bg.ca160281.png"
+  },
+  {
+    "revision": "a1df216b2c15cff09fce410a05bcc934",
+    "url": "/static/media/Exposure.a1df216b.png"
+  },
+  {
+    "revision": "4d131e79bd4a55a305146fc055a1620b",
+    "url": "/static/media/MainLogo.4d131e79.png"
+  },
+  {
+    "revision": "84abe14c9756256a4b91300ba3e4ec62",
+    "url": "/static/media/Raleway-Regular.84abe14c.ttf"
+  },
+  {
+    "revision": "afa29d298f928c9c6a91181b6b5d80ff",
+    "url": "/static/media/Service-Bg.afa29d29.png"
+  },
+  {
+    "revision": "5da8e3dba57e28e9035361487f652c11",
+    "url": "/static/media/YesevaOne-Regular.5da8e3db.ttf"
+  },
+  {
+    "revision": "e20a09ad309fd2db2b3bda460215b71c",
+    "url": "/static/media/vector-obj-1.e20a09ad.png"
+  }
+]);
